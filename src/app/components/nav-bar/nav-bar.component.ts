161fe0417d30f9db53e860fabf43224b03ff7e79
FernandoSullca <sullcafernando18@gmail.com>
2024-05-18 12:48:46 -0300

    Conexion basica al login de back Springboot

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
+import { DataService } from 'src/app/services/data/data.service';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent {
+export class NavBarComponent  implements OnInit {
+  isVisible = false;
+  user: any;
+  responseData: any;
 
+  constructor(private router: Router,private dataService : DataService ) { }
+
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart ) {
+        this.isVisible = event.url !== '/login';
+      }
+    });
+
+    this.dataService.responseData$.subscribe(data => {
+      this.responseData = data;
+      this.user=data;
+    });
+
+  }
 }

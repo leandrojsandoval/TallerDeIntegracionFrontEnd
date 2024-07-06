f30d3d8e331968daeda71f1bc3b0ed4dab207e93
Mauro <mauro97corrales@gmail.com>
2024-07-05 22:50:17 -0300

    exportar ventas

--- src/app/components/detalle-venta/detalle-venta.component.ts
+++ src/app/components/detalle-venta/detalle-venta.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { LineaDeVenta, Venta } from 'src/app/models';
 import { VentaService } from 'src/app/services/venta/venta.service';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-detalle-venta',
@@ -58,5 +59,47 @@ export class DetalleVentaComponent implements OnInit {
         return [...acumulador, valorActual];
       }
     }, []);
+  }
+  exportarDatos(): void {
+    const datosVenta = `Fecha: ${new Date(this.venta.fecha).toLocaleString()}\nCliente: ${this.venta.cliente}\n`;
+    const encabezado = `Código\tDescripción\tUnidades Vendidas\tRecaudacion\n`;
+    const datosProductos = this.LineasVentaproductoSinDuplicados.map(prod => 
+      `${prod.producto.codigo}\t${prod.producto.descripcion}\t${prod.cantidad}\t${prod.subtotal}\n`
+    ).join('');
+    const totalRecaudado = `Total Recaudado: ${this.Totales}\n`;
+
+    const contenido = datosVenta + encabezado + datosProductos + totalRecaudado;
+    const blob = new Blob([contenido], { type: 'text/plain' });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'detalle_venta.txt';
+    a.click();
+    window.URL.revokeObjectURL(url);
+  }
+  exportarDatosXLSX(): void {
+    const worksheetData = [
+      ['Fecha', new Date(this.venta.fecha).toLocaleString()],
+      ['Cliente', this.venta.cliente],
+      ['Total Recaudado', this.Totales],
+      [],
+      ['Código', 'Descripción', 'Unidades Vendidas', 'Recaudacion'],
+      ...this.LineasVentaproductoSinDuplicados.map(prod => [
+        prod.producto.codigo, prod.producto.descripcion, prod.cantidad, prod.subtotal
+      ]),
+    ];
+
+    const worksheet = XLSX.utils.aoa_to_sheet(worksheetData);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Detalle Venta');
+
+    const wbout = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+    const blob = new Blob([wbout], { type: 'application/octet-stream' });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'detalle_venta.xlsx';
+    a.click();
+    window.URL.revokeObjectURL(url);
   }
 }
